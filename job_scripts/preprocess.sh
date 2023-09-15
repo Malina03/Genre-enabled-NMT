2f4476793a4ed2d894c24d048115c719ca9aeb39
Malina03 <malina.chichirau@gmail>
2023-09-15 16:40:01 +0200

    fixed flag

--- job_scripts/preprocess.sh
+++ job_scripts/preprocess.sh
@@ -13,8 +13,6 @@ export PATH="$PATH:/home1/s3412768/.local/bin"
 module purge
 module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
 
-export PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:128
-export CUDA_VISIBLE_DEVICES=0 
 
 #load environment
 source /home1/s3412768/.envs/nmt2/bin/activate
@@ -26,5 +24,5 @@ if [ ! -d "$root_dir" ]; then
 fi
 
 python /home1/s3412768/Genre-enabled-NMT/src/preprocess_genre.py \
-    --lang $1 \
-    --df $root_dir \
+    -lang $1 \
+    -df $root_dir \
