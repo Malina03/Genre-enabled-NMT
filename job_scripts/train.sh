abf0a966a47221f382cdb69503b7aaaff3b35c95
Malina03 <malina.chichirau@gmail>
2023-10-09 18:51:07 +0200

    reduced time

--- job_scripts/train.sh
+++ job_scripts/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Job scheduling info, only for us specifically
-#SBATCH --time=72:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=train
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=1
