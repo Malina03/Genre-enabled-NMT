4774cb3714c29a80cab6b9c449f5890a7b624f36
Malina03 <malina.chichirau@gmail>
2023-10-16 17:37:49 +0200

    more mem

--- job_scripts/train.sh
+++ job_scripts/train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Job scheduling info, only for us specifically
-#SBATCH --time=10:00:00
+#SBATCH --time=36:00:00
 #SBATCH --job-name=train
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=1
-#SBATCH --mem=150G
+#SBATCH --mem=200G
 
 
 export PATH="$PATH:/home1/s3412768/.local/bin"
