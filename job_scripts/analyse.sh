0bfd10e496e83eab2f8598b6c0af5591a9107fc6
Malina03 <malina.chichirau@gmail>
2023-10-25 14:09:29 +0200

    test softmax

--- job_scripts/analyse.sh
+++ job_scripts/analyse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Job scheduling info, only for us specifically
-#SBATCH --time=00:15:00
+#SBATCH --time=00:30:00
 #SBATCH --job-name=prep
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=1
@@ -20,4 +20,6 @@ source /home1/s3412768/.envs/nmt2/bin/activate
 # root_dir="/scratch/s3412768/genre_NMT/en-hr/data"
 
 # python -u /home1/s3412768/Genre-enabled-NMT/src/analyse.py > $root_dir/analyse.log
-python -u /home1/s3412768/Genre-enabled-NMT/src/make_genre_datasets.py
+# python -u /home1/s3412768/Genre-enabled-NMT/src/make_genre_datasets.py
+
+python -u /home1/s3412768/Genre-enabled-NMT/src/x_genre_predict.py -lang hr -df /scratch/s3412768/genre_NMT/en-hr/data/softmax_saves
--- src/x_genre_predict.py
+++ src/x_genre_predict.py
@@ -5,6 +5,9 @@ from itertools import islice
 import time
 from tqdm import tqdm
 import pandas as pd
+import numpy as np
+from pathlib import Path
+import argparse
 
 # Install transformers
 # !pip install -q transformers
@@ -14,8 +17,17 @@ import pandas as pd
 
 from simpletransformers.classification import ClassificationModel
 
+def create_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-lang', '--lang_code', type=str, required=True, help='Language code of the target language')
+    parser.add_argument('-len', '--length_threshold', type=int, default=25, help='Minimum length of the documents used for genre classification')
+    parser.add_argument('-df', "--data_folder", type=str, default='data/', help='Folder where the data is stored')
+    args = parser.parse_args()
+    return args
 
-def predict(model, dataframe, final_file, dataframe_column="en_doc"):
+
+
+def predict(model, dataframe, final_file, dataframe_column="en_doc", softmax=False):
     """
         The function takes the dataframe with text in column dataframe_column, creates batches of 8,
         and applies genre predictions on batches, for faster prediction.
@@ -28,11 +40,14 @@ def predict(model, dataframe, final_file, dataframe_column="en_doc"):
         - final_file: the name of the final file with predictions
 
     """
+    labels = ["Other", "Information/Explanation", "News", "Instruction", "Opinion/Argumentation", "Forum", "Prose/Lyrical", "Legal", "Promotion"]
+
         # Split the dataframe into batches
         # Create batches of text
     def chunk(arr_range, arr_size):
         arr_range = iter(arr_range)
         return iter(lambda: tuple(islice(arr_range, arr_size)), ())
+    
 
     batches_list = list(chunk(dataframe[dataframe_column], 8))
 
@@ -44,6 +59,9 @@ def predict(model, dataframe, final_file, dataframe_column="en_doc"):
     print("The dataset is split into {} batches of {} texts.".format(len(batches_list_new),len(batches_list_new[0])))
 
     y_pred = []
+    y_distr = []
+    most_probable = []
+
 
     print("Prediction started.")
     start_time = time.time()
@@ -51,31 +69,55 @@ def predict(model, dataframe, final_file, dataframe_column="en_doc"):
     batches = len(batches_list_new)
     curr_batch = 0
 
-    ## added to finish timedout run
-    # curr_batch = 1000*17+1
-    # batches_list_new = batches_list_new[curr_batch:]
-    # y_pred = [0]*8*curr_batch
 
     for i in batches_list_new:
-        if curr_batch % 1000 == 0:
+        if curr_batch % 100 == 0 and curr_batch != 0:
             print("Predicting batch {} out of {}.".format(curr_batch, batches))
             # save the dataframe with predictions every 1000 batches
             # copy first current batch to a new dataframe
             dat = dataframe.iloc[(curr_batch-1000)*8:curr_batch*8]
             dat["X-GENRE"] = y_pred[(curr_batch-1000)*8:curr_batch*8]
+            if softmax == True:
+                dat["label_distribution"] = y_distr[(curr_batch-1000)*8:curr_batch*8]
+                dat["chosen_category_distr"] = most_probable[(curr_batch-1000)*8:curr_batch*8]
             dat.to_csv("{}_{}".format(final_file, curr_batch/1000), sep="\t")
-            del dat
-        
-        output = model.predict(i)
-        current_y_pred = [model.config.id2label[i] for i in output[0]]
+            # del dat
+            return dat
 
+        output = model.predict(i)
+        current_y_pred = output[0]
+        current_y_distr = output[1]
+        current_y_distr_softmax = []
+        current_y_distr_most_probable = []
+
+                
         for i in current_y_pred:
             y_pred.append(i)
+        
+        if softmax == True:
+            for i in current_y_distr:
+                distr = softmax(i)
+                distr_dict = {labels[i]: round(distr[i],4) for i in range(len(labels))}
+                current_y_distr_softmax.append(distr_dict)
+                # Also add the information for the softmax of the most probable category ("certainty")
+                distr_sorted = np.sort(distr)
+                current_y_distr_most_probable.append(distr_sorted[-1])
+
+            
+            for i in current_y_distr_softmax:
+                y_distr.append(i)
+            
+            for i in current_y_distr_most_probable:
+                most_probable.append(i)
+        
         curr_batch += 1
 
     # save the final batch of predictions
     dat = dataframe.iloc[(curr_batch-1000)*8:((curr_batch-1000)*8+len(current_y_pred))]
     dat["X-GENRE"] = y_pred[(curr_batch-1000)*8:((curr_batch-1000)*8+len(current_y_pred))]
+    if softmax == True:
+        dat["label_distribution"] = y_distr[(curr_batch-1000)*8:((curr_batch-1000)*8+len(current_y_pred))]
+        dat["mos_probable"] = most_probable[(curr_batch-1000)*8:((curr_batch-1000)*8+len(current_y_pred))]
     dat.to_csv("{}_{}".format(final_file, int(curr_batch/1000)), sep="\t")
     del dat
 
@@ -94,16 +136,7 @@ def predict(model, dataframe, final_file, dataframe_column="en_doc"):
 
     return res
 
-
-    # # dataframe["X-GENRE"] = y_pred
-
-    # # Save the new dataframe which contains the y_pred values as well
-    # dataframe.to_csv("{}".format(final_file), sep="\t")
-
-    # return dataframe
-
-
-def classify_dataset(data, tgt_column, save_file):
+def classify_dataset(data, tgt_column, save_file, softmax=False):
     """
         The function takes the dataframe with text in column dataframe_column, creates batches of 8,
         and applies genre predictions on batches, for faster prediction.
@@ -133,8 +166,45 @@ def classify_dataset(data, tgt_column, save_file):
         args=model_args
     )
 
-    labelled = predict(model, data, save_file, tgt_column)
+    labelled = predict(model, data, save_file, tgt_column, softmax=softmax)
     # remove all columns except en_doc and X-GENRE to save memory
-    labelled = labelled[["en_doc", "X-GENRE"]]
+    if softmax == False:
+        labelled = labelled[["en_doc", "X-GENRE"]]
+    else:
+        labelled = labelled[["en_doc", "X-GENRE", "label_distribution", "chosen_category_distr"]]
     return labelled
 
+    # Apply softmax to the raw outputs
+def softmax(x):
+    '''Compute softmax values for each sets of scores in x.'''
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def main():
+    '''Makes predictions on the dataset returning the full output, label distribution and most probable label.'''
+    args = create_arg_parser()
+    data_folder = Path(args.data_folder)
+    # Load the dataframe
+    data= pd.read_csv(data_folder/f"Macocu-{args.lang_code}-en-doc-format-duplicates.csv", sep="\t", header=0)
+    # only use docs with length >= args.length_threshold
+    data = data[data['en_length'] >= args.length_threshold]
+    # only use unique docs for labelling to save time
+    data = data.drop_duplicates("en_doc")
+    # only save the en_doc column to save memory
+    data = data[["en_doc"]]
+    print("Labelling started. Using docs with length >= {}".format(args.length_threshold))
+    doc_labels = classify_dataset(data, "en_doc", data_folder/f'Macocu-{args.lang_code}-en.labelled.softmax{args.length_threshold}.csv', softmax=True)
+    print(f"Labelling done. Saving the labelled data to {args.data_folder}/Macocu-{args.lang_code}-en.doc.labels.softmax.{args.length_threshold}.csv")
+    # Combine the sentence level data and doc_labels
+    print(f"Combining the sentence level data and doc_labels. Saving the combined data to {args.data_folder}/Macocu-{args.lang_code}-en-sent-doc-labelled-softmax.csv")
+    # load the full dataset again to get all all sentences back 
+    del data
+    data= pd.read_csv(data_folder/f"Macocu-{args.lang_code}-en-doc-format-duplicates.csv", sep="\t", header=0)
+    # merge doc_data and data based on en_doc
+    data = pd.merge(doc_labels, data, on="en_doc")
+    data = data.drop(columns=["Unnamed: 0"])
+    data.to_csv(f"{args.data_folder}/Macocu-{args.lang_code}-en-sent-doc-labelled-softmax.csv", sep="\t", index=False) 
+
+
+    if __name__ == "__main__":
+        main()
