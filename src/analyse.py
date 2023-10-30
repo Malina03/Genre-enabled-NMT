3d34033107342d9b078773f9dbf6e676a913bee0
Malina03 <malina.chichirau@gmail>
2023-10-30 19:27:30 +0100

    updated analyse

--- src/analyse.py
+++ src/analyse.py
@@ -12,6 +12,10 @@ def print_genre_distribution(data):
 
 def main():
     data = pd.read_csv('/scratch/s3412768/genre_NMT/en-hr/data/MaCoCu.en-hr_complete.tsv', sep='\t')
+    labels_distr = pd.read('/scratch/s3412768/genre_NMT/en-hr/data/softmax_saves/Macocu-hr-en-sent-doc-labelled-softmax.csv', sep='\t')
+    # remove all but en_par, X-GENRE, "label_distribution", "chosen_category_distr"
+    labels_distr = labels_distr[['en_par', 'X-GENRE', 'label_distribution', 'chosen_category_distr']]
+    data = pd.merge(data, labels_distr, on='en_par')
     # check if the sources are different by set
     dev_src = data[data['set']=='dev']['en_domain'].unique()
     test_src = data[data['set']=='test']['en_domain'].unique()
@@ -31,6 +35,45 @@ def main():
 
     print("\n\n\n")
 
+    # average chosen category distribution per genre
+    print("Average chosen category distribution per genre:")
+    print(data.groupby('X-GENRE')['chosen_category_distr'].mean())
+    # median chosen category distribution per genre
+    print("Median chosen category distribution per genre:")
+    # average chosen category distribution per genre per set
+    print("Average chosen category distribution per genre per set:")
+    print(data.groupby(['X-GENRE', 'set'])['chosen_category_distr'].mean())
+    # median chosen category distribution per genre per set
+    print("Median chosen category distribution per genre per set:")
+    print(data.groupby(['X-GENRE', 'set'])['chosen_category_distr'].median())
+
+    print("\n\n\n")
+    # number of labels of each genre with above 0.9 confidence
+    print("Number of labels of each genre with above 0.9 confidence:")
+    print(data[data['chosen_category_distr']>=0.9].groupby('X-GENRE')['chosen_category_distr'].count())
+
+    # add columns with the confidence of each label from the label distribution
+    labels = ["Other", "Information/Explanation", "News", "Instruction", "Opinion/Argumentation", "Forum", "Prose/Lyrical", "Legal", "Promotion"]
+
+    for i in range(0, 10):
+        data['label_'+labels[i]+'_conf'] = data['label_distribution'].apply(lambda x: x[i])
+    
+    # make column for 2nd most confident category by comparing the label confidences
+    data['2nd_most_confident_dist'] = data.apply(lambda x: sorted([x['label_'+labels[i]+'_conf'] for i in range(0, 10)], reverse=True)[1], axis=1)
+    data['2nd_most_confident'] = data.apply(lambda x: labels[[x['label_'+labels[i]+'_conf'] for i in range(0, 10)].index(sorted([x['label_'+labels[i]+'_conf'] for i in range(0, 10)], reverse=True)[1])], axis=1)
+    # average 2nd most confident category distribution per genre
+    print("Average 2nd most confident category distribution per genre:")
+    print(data.groupby('X-GENRE')['2nd_most_confident_dist'].mean())
+    # median 2nd most confident category distribution per genre
+    print("Median 2nd most confident category distribution per genre:")
+    print(data.groupby('X-GENRE')['2nd_most_confident_dist'].median())
+
+    # the 2nd most likely label per genre 
+    print("The 2nd most likely label per genre:")
+    print(data.groupby('X-GENRE')['2nd_most_confident'].value_counts())
+
+    print("\n\n\n")
+
     data = data.drop_duplicates(subset=['en_doc'])
     print ("Number of docs in the dataset:", len(data))
     print("Gnre distribution in the entire dataset:")
@@ -41,6 +84,8 @@ def main():
     print(data[data['set']=='dev']['X-GENRE'].value_counts())
     print("Genre distribution in the test set:")
     print(data[data['set']=='test']['X-GENRE'].value_counts())
+
+    print("\n\n\n")
 
 if __name__ == "__main__":
     main()
