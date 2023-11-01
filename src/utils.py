2f628215a9a87630c690a3e2f73f5cdd010ef916
Malina03 <malina.chichirau@gmail>
2023-11-01 19:10:41 +0100

    check tokenizer type

--- src/utils.py
+++ src/utils.py
@@ -18,7 +18,7 @@ def get_args():
     parser.add_argument("-predict", "--predict", required=False, action="store_true", help="Whether to only predict with the model.")
     parser.add_argument("-exp_type", "--exp_type", required=False, type=str, default="fine_tune", help="Type of experiment.Can be fine_tuned or from_scratch.")
     parser.add_argument("-model_type", "--model_type", required=False, type=str, default=None, help="Type of model. genre_aware, genre_aware_token, baseline, doc-")
-    parser.add_argument("-genre", "--genre", required=False, type=str, default='', help="Genre used for fine tuning.")
+    parser.add_argument("-genre", "--genre", required=False, type=str, default=None, help="Genre used for fine tuning.")
     parser.add_argument("-wandb", "--wandb", required=False, action="store_true", help="Whether to log the training process on wandb.")
     parser.add_argument("-eval_baseline", "--eval_baseline", required=False, action="store_true", help="Whether to evaluate the baseline model before fine-tuning.")
 
@@ -76,6 +76,7 @@ def train_tokenizer(args, tokenizer_batch=1000):
     tags = ['<info>', '<promo>', '<news>', '<law>', '<other>', '<arg>', '<instr>', '<lit>', '<forum>']
     data = load_tokenizer_data(args.train_file)
     tokenizer = AutoTokenizer.from_pretrained(args.model_name, max_len=args.max_length, truncation=True, padding=True)
+    print("Tokenizer is fast tokenizer: ", tokenizer.is_fast)
     tokenizer.train_from_iterator(batch_generator(data, tokenizer_batch), vocab_size=tokenizer.vocab_size, new_special_tokens=tags if 'genre_aware_token' in args.model_type else None)
     save_path = args.tokenizer_path if args.tokenizer_path else os.path.join(args.root_dir, "models", args.exp_type, args.model_type, "tokenizer")
     tokenizer.save_pretrained(save_path)
