01a137a388192b577872e6d418e572976edd3323
Malina03 <malina.chichirau@gmail>
2023-11-03 12:57:21 +0100

    ignore pad to multiples of

--- src/train.py
+++ src/train.py
@@ -50,11 +50,11 @@ if __name__ == "__main__":
             config = AutoConfig.from_pretrained(args.model_name)
             model = AutoModelForSeq2SeqLM.from_config(config)
             if train_tokenizer:
-                model.resize_token_embeddings(len(tokenizer), pad_to_multiple_of=16)
+                model.resize_token_embeddings(len(tokenizer))
         else:
             model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name)
         if "genre_aware_token" in args.model_type:
-            model.resize_token_embeddings(len(tokenizer), pad_to_multiple_of=16)
+            model.resize_token_embeddings(len(tokenizer))
     else:
         model = AutoModelForSeq2SeqLM.from_pretrained(args.checkpoint, local_files_only=True)
 
