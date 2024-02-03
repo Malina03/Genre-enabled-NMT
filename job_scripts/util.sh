951833b46cda57d4797c0dca1cafa0144bb811bb
Malina03 <malina.chichirau@gmail>
2024-02-03 10:53:23 +0100

    updated

--- job_scripts/util.sh
+++ job_scripts/util.sh
@@ -14,8 +14,8 @@ module purge
 module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
 
 
-# start_batch=$1
-# end_batch=$2
+start_batch=$1
+end_batch=$2
 
 #load environment
 source /home1/s3412768/.envs/nmt2/bin/activate
@@ -24,6 +24,6 @@ root_dir="/scratch/s3412768/genre_NMT/en-is/data"
 
 # python -u /home1/s3412768/Genre-enabled-NMT/src/change_genre_tokens.py
 # python -u /home1/s3412768/Genre-enabled-NMT/src/analyse.py > $root_dir/analyse.log
-python -u /home1/s3412768/Genre-enabled-NMT/src/make_genre_datasets.py
+# python -u /home1/s3412768/Genre-enabled-NMT/src/make_genre_datasets.py
 
-# python -u /home1/s3412768/Genre-enabled-NMT/src/x_genre_predict.py -lang hr -df /scratch/s3412768/genre_NMT/en-hr/data/softmax_saves -sb $start_batch -eb $end_batch 
+python -u /home1/s3412768/Genre-enabled-NMT/src/x_genre_predict.py -lang tr -df /scratch/s3412768/genre_NMT/en-hr/data/softmax_saves -sb $start_batch -eb $end_batch 
