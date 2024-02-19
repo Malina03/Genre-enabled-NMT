1f8e3023eede73df907cd6073413d7a7bf06c527
Malina03 <malina.chichirau@gmail>
2024-02-19 14:18:37 +0100

    make instr data

--- job_scripts/util.sh
+++ job_scripts/util.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Job scheduling info, only for us specifically
-#SBATCH --time=00:20:00
+#SBATCH --time=01:00:00
 #SBATCH --job-name=analyse
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=1
-#SBATCH --mem=164G
+#SBATCH --mem=50G
 
 
 export PATH="$PATH:/home1/s3412768/.local/bin"
@@ -23,7 +23,7 @@ source /home1/s3412768/.envs/nmt2/bin/activate
 root_dir="/scratch/s3412768/genre_NMT/en-tr/data"
 
 # python -u /home1/s3412768/Genre-enabled-NMT/src/change_genre_tokens.py
-python -u /home1/s3412768/Genre-enabled-NMT/src/analyse.py > $root_dir/analyse.log
-# python -u /home1/s3412768/Genre-enabled-NMT/src/make_genre_datasets.py 
+# python -u /home1/s3412768/Genre-enabled-NMT/src/analyse.py > $root_dir/analyse.log
+python -u /home1/s3412768/Genre-enabled-NMT/src/make_genre_datasets.py 
 
 # python -u /home1/s3412768/Genre-enabled-NMT/src/x_genre_predict.py -lang tr -df /scratch/s3412768/genre_NMT/en-tr/data/softmax_saves -sb $start_batch -eb $end_batch 
