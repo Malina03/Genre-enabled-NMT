35c1912293556a6b7e3dd399e57e6f22844bf3f5
Malina03 <malina.chichirau@gmail>
2024-02-21 16:35:20 +0100

    fixed files

--- job_scripts/eval/predict_eval_opus_doc.sh
+++ job_scripts/eval/predict_eval_opus_doc.sh
@@ -98,13 +98,15 @@ out=$root_dir/eval/$exp_type/$model_type/${out_file}_predictions.txt
 
 # split file 
 
-python /home1/s3412768/Genre-enabled-NMT/src/split_docs_for_eval.py --input_file $out  --output_dir $out.split
+python /home1/s3412768/Genre-enabled-NMT/src/split_docs_for_eval.py --input_file $out  --output_file $out.split
 
 # eval on the file wothout docs
 eval="$root_dir/data/${out_file}.en-$language.test.tsv"
 
 
-echo "Output file: $out"
+echo "Original Output file: $out"
+out=${out}.split
+echo "Split file for evaluation: $out.split"
 echo "Eval file: $eval"
 
 ref=${eval}.ref
