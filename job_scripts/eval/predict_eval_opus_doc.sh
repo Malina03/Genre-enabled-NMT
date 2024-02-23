a760b15a5d1f505efdc29e5a4b28d2917d219f6a
Malina03 <malina.chichirau@gmail>
2024-02-23 13:38:57 +0100

    pred eval opus doc

--- job_scripts/eval/predict_eval_opus_doc.sh
+++ job_scripts/eval/predict_eval_opus_doc.sh
@@ -71,94 +71,94 @@ echo "log file: $log_file"
 echo "model type: $model_type"
 echo "model: $model"
 
-# python /home1/s3412768/Genre-enabled-NMT/src/train.py \
-#     --root_dir $root_dir \
-#     --train_file $test_file \
-#     --dev_file $test_file \
-#     --test_file $test_file \
-#     --gradient_accumulation_steps 1 \
-#     --batch_size 32 \
-#     --gradient_checkpointing \
-#     --adafactor \
-#     --save_strategy epoch \
-#     --evaluation_strategy epoch \
-#     --learning_rate 1e-4 \
-#     --exp_type $exp_type \
-#     --model_type $model_type \
-#     --model_name $model \
-#     --predict \
-#     --eval \
-#     &> $log_file
+python /home1/s3412768/Genre-enabled-NMT/src/train.py \
+    --root_dir $root_dir \
+    --train_file $test_file \
+    --dev_file $test_file \
+    --test_file $test_file \
+    --gradient_accumulation_steps 1 \
+    --batch_size 32 \
+    --gradient_checkpointing \
+    --adafactor \
+    --save_strategy epoch \
+    --evaluation_strategy epoch \
+    --learning_rate 1e-4 \
+    --exp_type $exp_type \
+    --model_type $model_type \
+    --model_name $model \
+    --predict \
+    --eval \
+    &> $log_file
 
 
-eval_file=$test_on
-out_file="$(cut -d'.' -f1 <<<"$test_on")"
+# eval_file=$test_on
+# out_file="$(cut -d'.' -f1 <<<"$test_on")"
 
-out=$root_dir/eval/$exp_type/$model_type/${out_file}_predictions.txt
+# out=$root_dir/eval/$exp_type/$model_type/${out_file}_predictions.txt
 
-# split file 
+# # split file 
 
-# python /home1/s3412768/Genre-enabled-NMT/src/split_docs_for_eval.py --input_file $out  --output_file $out.split
+# # python /home1/s3412768/Genre-enabled-NMT/src/split_docs_for_eval.py --input_file $out  --output_file $out.split
 
-# eval on the file wothout docs
-eval="$root_dir/data/${out_file}.en-$language.doc.test.tsv"
-
-
-echo "Output file: $out"
-# out=${out}
-echo "Eval file: $eval"
-
-ref=${eval}.ref
-src=${eval}.src
+# # eval on the file wothout docs
+# eval="$root_dir/data/${out_file}.en-$language.doc.test.tsv"
 
 
-# deactivate the env used for predictions
-deactivate
-# remove the module used for predictions and load the new one
-module purge
-module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
-source $HOME/.envs/nmt_eval/bin/activate
-set -eu -o pipefail
+# echo "Output file: $out"
+# # out=${out}
+# echo "Eval file: $eval"
+
+# ref=${eval}.ref
+# src=${eval}.src
 
 
-# check if ref and src files exist and create them if not
-if [[ ! -f $ref ]]; then
-    echo "Reference file $ref not found, create it"
-    # First check if the file exists in the data folder
-    if [[ -f $eval ]]; then
-        # If so, extract the reference column
-        cut -d $'\t' -f2 $eval > "$ref"
-    else
-        echo "File $eval not found"
-    fi
-fi
+# # deactivate the env used for predictions
+# deactivate
+# # remove the module used for predictions and load the new one
+# module purge
+# module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
+# source $HOME/.envs/nmt_eval/bin/activate
+# set -eu -o pipefail
 
-# if [[ ! -f $src ]]; then
-#     echo "Source file $src not found, create it"
+
+# # check if ref and src files exist and create them if not
+# if [[ ! -f $ref ]]; then
+#     echo "Reference file $ref not found, create it"
 #     # First check if the file exists in the data folder
 #     if [[ -f $eval ]]; then
-#         # If so, extract the source column
-#         cut -d $'\t' -f1 $eval > "$src"
+#         # If so, extract the reference column
+#         cut -d $'\t' -f2 $eval > "$ref"
 #     else
 #         echo "File $eval not found"
 #     fi
 # fi
 
+# # if [[ ! -f $src ]]; then
+# #     echo "Source file $src not found, create it"
+# #     # First check if the file exists in the data folder
+# #     if [[ -f $eval ]]; then
+# #         # If so, extract the source column
+# #         cut -d $'\t' -f1 $eval > "$src"
+# #     else
+# #         echo "File $eval not found"
+# #     fi
+# # fi
 
-if [[ ! -f $out ]]; then
-    echo "Output file $out not found, skip evaluation"
-else
 
-    # First put everything in 1 file
-    sacrebleu $out -i $ref -m bleu ter chrf --chrf-word-order 2 > "${out}.eval.sacre"
-    # Add chrf++ to the previous file
-    sacrebleu $out -i $ref -m chrf --chrf-word-order 2 >> "${out}.eval.sacre"
-    # Write only scores to individual files
-    sacrebleu $out -i $ref -m bleu -b > "${out}.eval.bleu"
-    sacrebleu $out -i $ref -m ter -b > "${out}.eval.ter"
-    sacrebleu $out -i $ref -m chrf -b > "${out}.eval.chrf"
-    sacrebleu $out -i $ref -m chrf --chrf-word-order 2 -b > "${out}.eval.chrfpp"
+# if [[ ! -f $out ]]; then
+#     echo "Output file $out not found, skip evaluation"
+# else
+
+#     # First put everything in 1 file
+#     sacrebleu $out -i $ref -m bleu ter chrf --chrf-word-order 2 > "${out}.eval.sacre"
+#     # Add chrf++ to the previous file
+#     sacrebleu $out -i $ref -m chrf --chrf-word-order 2 >> "${out}.eval.sacre"
+#     # Write only scores to individual files
+#     sacrebleu $out -i $ref -m bleu -b > "${out}.eval.bleu"
+#     sacrebleu $out -i $ref -m ter -b > "${out}.eval.ter"
+#     sacrebleu $out -i $ref -m chrf -b > "${out}.eval.chrf"
+#     sacrebleu $out -i $ref -m chrf --chrf-word-order 2 -b > "${out}.eval.chrfpp"
     
-    # comet-score -s $src -t $out -r $ref > "${out}.eval.comet"
+#     # comet-score -s $src -t $out -r $ref > "${out}.eval.comet"
 
-fi
+# fi
