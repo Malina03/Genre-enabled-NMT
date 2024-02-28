124437af752608a537907b807e7c583e6a2f696a
Malina03 <malina.chichirau@gmail>
2024-02-28 19:45:27 +0100

    util

--- job_scripts/util.sh
+++ job_scripts/util.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Job scheduling info, only for us specifically
-#SBATCH --time=00:20:00
+#SBATCH --time=00:10:00
 #SBATCH --job-name=analyse
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=1
@@ -23,7 +23,7 @@ source /home1/s3412768/.envs/nmt2/bin/activate
 root_dir="/scratch/s3412768/genre_NMT/en-tr/data"
 
 # python -u /home1/s3412768/Genre-enabled-NMT/src/change_genre_tokens.py
-# python -u /home1/s3412768/Genre-enabled-NMT/src/analyse.py > $root_dir/analyse.log
-python -u /home1/s3412768/Genre-enabled-NMT/src/make_genre_datasets.py > $root_dir/make_genre_datasets.log
+python -u /home1/s3412768/Genre-enabled-NMT/src/analyse.py > $root_dir/analyse_extra.log
+# python -u /home1/s3412768/Genre-enabled-NMT/src/make_genre_datasets.py > $root_dir/make_genre_datasets.log
 
 # python -u /home1/s3412768/Genre-enabled-NMT/src/x_genre_predict.py -lang tr -df /scratch/s3412768/genre_NMT/en-tr/data/softmax_saves -sb $start_batch -eb $end_batch 
