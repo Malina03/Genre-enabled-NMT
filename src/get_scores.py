8389b48296c3cc9233d6c6c2d47face961b489f4
Malina03 <malina.chichirau@gmail>
2024-03-06 12:44:08 +0100

    scores across all seeds and genres at once

--- src/get_scores.py
+++ src/get_scores.py
@@ -90,12 +90,12 @@ for f in eval_files:
         print(len(individial_scores_comet))
         scores_per_genre = pd.DataFrame({'model': [model]*len(genres), 'test_file': [f_name]*len(genres), 'genre': genres, 'comet': individial_scores_comet})
         comet_scores_per_genre = comet_scores_per_genre.append(scores_per_genre, ignore_index=True)
-        # compute the average score per genre and standard deviation
-        scores_per_genre = scores_per_genre.groupby(['model', 'test_file', 'genre']).agg({'comet': ['mean', 'std']}).reset_index()
-        scores_per_genre.columns = ['model', 'test_file', 'genre', 'comet_mean', 'comet_std']
-        # split model name in model and seed
+        # compute the average score per genre and standard deviation across ALL SEEDS at once
         scores_per_genre['seed'] = scores_per_genre['model'].str[-1]
         scores_per_genre['model'] = scores_per_genre['model'].str[:-2]
+        scores_per_genre = scores_per_genre.groupby(['model', 'test_file', 'genre']).agg({'comet': ['mean', 'std']}).reset_index()
+        scores_per_genre.columns = ['model', 'test_file', 'genre', 'comet_mean', 'comet_std']
+
         # compute the average score per model and standard deviation
         scores_per_model = scores_per_genre.groupby(['model', 'test_file', 'genre']).agg({'comet_mean': ['mean', 'std']}).reset_index()
         scores_per_model.columns = ['model', 'test_file', 'genre', 'comet_mean', 'comet_std']
