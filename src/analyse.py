0678e0eee650999ce1519bee95ff3e1eb5a8538c
Malina03 <malina.chichirau@gmail>
2024-03-12 16:21:22 +0100

    hr

--- src/analyse.py
+++ src/analyse.py
@@ -41,7 +41,7 @@ def main():
 
 
 
-    data = pd.read_csv('/scratch/s3412768/genre_NMT/en-tr/data/MaCoCu.en-tr.doc_complete.tsv', sep='\t', header=0)
+    data = pd.read_csv('/scratch/s3412768/genre_NMT/en-hr/data/MaCoCu.en-hr.doc_complete.tsv', sep='\t', header=0)
 
 
 
@@ -173,9 +173,9 @@ def main():
     # print("\n\n\n")
 
     data = data.drop_duplicates(subset=['en_doc'])
-    data['tr_doc_length'] = data['tr_doc'].apply(lambda x: len(x.split(' ')))
+    data['hr_doc_length'] = data['hr_doc'].apply(lambda x: len(x.split(' ')))
     data['en_doc_length'] = data['en_doc'].apply(lambda x: len(x.split(' ')))
-    print("Average document length in Turkish: {} and std {} and median {}".format(data['tr_doc_length'].mean(), data['tr_doc_length'].std()), data['tr_doc_length'].median())
+    print("Average document length in Croatian: {} and std {} and median {}".format(data['hr_doc_length'].mean(), data['hr_doc_length'].std()), data['hr_doc_length'].median())
     print("Average document length in English: {} and std {} and median {}".format(data['en_doc_length'].mean(), data['en_doc_length'].std()), data['en_doc_length'].median())
     print ("Number of docs in the dataset:", len(data))
     print("Gnre distribution in the entire dataset:")
